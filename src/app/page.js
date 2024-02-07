77163f5590d02352be09b57f13171b6c986a5bd5
Lycopenee <309334941@qq.com>
2024-02-06 21:56:42 -0600

    update the google analysis support

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,26 @@
 
 import Profile from "./profile";
 import Cardlist from "./cardlist";
+import Head from "next/head";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 // home page 
 export default function Home() {
   return (
-    
-    <div className="md:grid place-content-center ">
-      <main className=" ">
-        <div id="profile" className=" flex place-content-center ">
-          <Profile/>
-        </div>
-        <div id="cardlist" className="flex max-w-4xl place-content-center">
-          <Cardlist/>
-        </div>
-      </main> 
-    </div>
+    <>
+      <Head>
+        <GoogleAnalytics gaId="G-5K7T3SZ06Z" />
+      </Head>
+      <div className="md:grid place-content-center ">
+        <main className=" ">
+          <div id="profile" className=" flex place-content-center ">
+            <Profile />
+          </div>
+          <div id="cardlist" className="flex max-w-4xl place-content-center">
+            <Cardlist />
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
