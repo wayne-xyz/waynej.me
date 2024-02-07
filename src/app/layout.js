45a49c0fcbee64e2b514b6decca892031e7eb088
Lycopenee <309334941@qq.com>
2024-02-06 22:36:40 -0600

    update the google analy func

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from "next/script";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +14,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <GoogleAnalytics gaId="G-5K7T3SZ06Z" />
+        <Script id="google-analytics">
+          {` 
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-5K7T3SZ06Z');
+      `}
+        </Script>
+      </head>
       <body className={inter.className}>{children}</body>
       <GoogleAnalytics gaId="G-5K7T3SZ06Z" />
     </html>
