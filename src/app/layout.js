95dfed89d9585734adbcb78501066be118a0c8e0
Lycopenee <309334941@qq.com>
2024-02-06 22:18:20 -0600

    update the google analytics part

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-5K7T3SZ06Z" />
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,10 @@
 import Profile from "./profile";
 import Cardlist from "./cardlist";
 import Head from "next/head";
-import { GoogleAnalytics } from '@next/third-parties/google'
 
 // home page 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <GoogleAnalytics gaId="G-5K7T3SZ06Z" />
-      </Head>
       <div className="md:grid place-content-center ">
         <main className=" ">
           <div id="profile" className=" flex place-content-center ">
@@ -21,6 +16,5 @@ export default function Home() {
           </div>
         </main>
       </div>
-    </>
   );
 }
