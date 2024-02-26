07a59d296a4b0aa8dbba3e7d4511ce6d215b4abd
livingspring <309334941@qq.com>
2024-02-26 10:07:37 -0600

    update the dark and light background color

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import Backgournd from "./background";
 // home page 
 export default function Home() {
   return (
-      <div className="mb-0  md:grid place-content-center bg-gradient-to-tl  from-darkbg-blue-100 from-30% via-darkbg-blue-300 via-60% to-darkbg-blue-200 to-100% rounded-lg " >
+      <div className="mb-0  md:grid place-content-center bg-gradient-to-tl from-lightbg-blue-100 via-lightbg-blue-200 to-lightbg-blue-300  dark:from-darkbg-blue-100 from-30% dark:via-darkbg-blue-300 via-60% dark:to-darkbg-blue-200 to-100% rounded-lg " >
         <main className="mb-20 ">
           <div id="profile" className=" flex place-content-center ">
             <Profile />
