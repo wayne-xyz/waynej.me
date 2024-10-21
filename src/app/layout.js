799ac7bdf1dd4a0cc4113822c6d0a80a461eb859
Lycopenee <309334941@qq.com>
2024-10-21 03:37:34 -0500

    released

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,25 +1,26 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { GoogleAnalytics } from '@next/third-parties/google'
-import Script from "next/script";
-import Head from "next/head";
+import { ThemeProvider } from 'next-themes'
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Wayne",
-  description: "To be next",
+  description: "Dev For Fun",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <main >
-        {children}
-        </main>
-        </body>
-      <GoogleAnalytics gaId="G-5K7T3SZ06Z" />
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <main>
+            {children}
+          </main>
+        </ThemeProvider>
+        <GoogleAnalytics gaId="G-5K7T3SZ06Z" />
+      </body>
     </html>
   );
 }
